6bde1d181bfd6950b8d782fff504da4dfbabacae
illumiko <eyanat.rayannjr@gmail.com>
2023-08-17 15:15:02 +0600

    Fixed dap keymap bug

--- after/plugin/debugger.lua
+++ after/plugin/debugger.lua
@@ -1,16 +1,15 @@
-local dap = require('dap')
 require('dapui').setup()
 
-vim.keymap.set('n', '<leader>ds', function() dap.continue() end)
-vim.keymap.set('n', '<leader>do', function() dap.step_over() end)
-vim.keymap.set('n', '<leader>dj', function() dap.step_into() end)
-vim.keymap.set('n', '<leader>dk', function() dap.step_out() end)
-vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
-vim.keymap.set('n', '<Leader>B', function() dap.set_breakpoint() end)
-vim.keymap.set('n', '<Leader>lp', function() dap.
+vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end)
+vim.keymap.set('n', '<leader>do', function() require('dap').step_over() end)
+vim.keymap.set('n', '<leader>dj', function() require('dap').step_into() end)
+vim.keymap.set('n', '<leader>dk', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+vim.keymap.set('n', '<Leader>lp', function() require('dap').
     set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
-vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
 
 vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
     require('dap.ui.widgets').hover()
@@ -31,6 +30,7 @@ vim.keymap.set('n', '<Leader>ds', function()
 end)
 
 
+local dap = require('dap')
 dap.adapters.python = {
     type = 'executable';
     command = '/home/miko/.virtualenvs/debugpy/bin/python3';
