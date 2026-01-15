3384ad3a8d647e4956b37daec2381198a3d01c13
illumiko <eyanat.rayannjr@gmail.com>
2026-01-16 00:21:38 +0600

    colorscheme change to arctic

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -2,6 +2,7 @@ require("user.keymaps")
 require("user.settings")
 
 vim.cmd("colorscheme nordbones") --giving windline sane colors with nordic
-vim.cmd("colorscheme nordic")
+-- vim.cmd("colorscheme nordic")
+vim.cmd("colorscheme arctic")
 
 require("user.highlights")
