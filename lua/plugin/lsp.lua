2d0fc29fb5b953f6c6e7d121b608d9abbb9d88de
illumiko <eyanat.rayannjr@gmail.com>
2026-02-28 01:29:36 +0600

    added rust lsp

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -1,48 +1,72 @@
 vim.lsp.enable({
-    "pyright",
-    "lua_ls",
-    "gopls",
-    "emmet_language_server",
-    'ts_ls',
-    'clangd'
+	-- "pyright",
+	"jedi_language_server",
+	"java_language_server",
+	"qmlls",
+	"lua_ls",
+	"gopls",
+	"emmet_language_server",
+	"ts_ls",
+	"clangd",
+    -- "rust_analyzer",
 })
-vim.lsp.config['emmet_language_server'] = {
-    filetypes = { "astro", "css", "eruby", "html", "htmlangular", "htmldjango", "javascriptreact", "less", "pug", "sass", "scss", "svelte", "templ", "typescriptreact", "vue", "tmpl","template" }
+vim.lsp.config["emmet_language_server"] = {
+	filetypes = {
+		"astro",
+		"css",
+		"eruby",
+		"html",
+		"htmlangular",
+		"htmldjango",
+		"javascriptreact",
+		"less",
+		"pug",
+		"sass",
+		"scss",
+		"svelte",
+		"templ",
+		"typescriptreact",
+		"vue",
+		"tmpl",
+		"template",
+	},
 }
-vim.lsp.config['gopls'] = {
-  settings = {
-    gopls = {
-      hints = {
-        rangeVariableTypes = true,
-        parameterNames = true,
-        constantValues = true,
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        functionTypeParameters = true,
-      },
-    }
-  }
-
+vim.lsp.config["qmlls"] = {
+	cmd = { "qmlls6", "-E" },
 }
-vim.diagnostic.config({virtual_text = {current_line=true}})
+vim.lsp.config["gopls"] = {
+	settings = {
+		gopls = {
+			hints = {
+				rangeVariableTypes = true,
+				parameterNames = true,
+				constantValues = true,
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				functionTypeParameters = true,
+			},
+		},
+	},
+}
+vim.diagnostic.config({ virtual_text = { current_line = true } })
 vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 return {
-    "neovim/nvim-lspconfig",
-  -- "MysticalDevil/inlay-hints.nvim",
-  --   event = "LspAttach",
-  --   dependencies = { "neovim/nvim-lspconfig" },
-  --   config = function()
-  --       require("inlay-hints").setup({
-  --           commands = {enabled= true},
-  --           autocmd = {enabled = true}
-  --       })
-  --   end
+	"neovim/nvim-lspconfig",
+	-- "MysticalDevil/inlay-hints.nvim",
+	--   event = "LspAttach",
+	--   dependencies = { "neovim/nvim-lspconfig" },
+	--   config = function()
+	--       require("inlay-hints").setup({
+	--           commands = {enabled= true},
+	--           autocmd = {enabled = true}
+	--       })
+	--   end
 
-{
-	"chrisgrieser/nvim-lsp-endhints",
-	event = "LspAttach",
-	opts = {}, -- required, even if empty
-},
-"RRethy/vim-illuminate"
+	{
+		"chrisgrieser/nvim-lsp-endhints",
+		event = "LspAttach",
+		opts = {}, -- required, even if empty
+	},
+	"RRethy/vim-illuminate",
 }
---
+++ lua/plugin/rustlsp.lua
@@ -0,0 +1,7 @@
+local M = {}
+M.lazy = {
+	"mrcjkb/rustaceanvim",
+	version = "^8", -- Recommended
+	lazy = false, -- This plugin is already lazy
+}
+return M.lazy
