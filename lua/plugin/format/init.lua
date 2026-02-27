785fccddd35b84fab630a06e731026eb51c95995
illumiko <eyanat.rayannjr@gmail.com>
2026-02-28 01:28:17 +0600

    disabled format.nvim constant logging

--- lua/plugin/format/init.lua
+++ lua/plugin/format/init.lua
@@ -3,9 +3,9 @@ local config = function()
 	local util = require("formatter.util")
 
 	require("formatter").setup({
-      logging = true,
-      -- Set the log level
-      log_level = vim.log.levels.WARN,
+		logging = false,
+		-- Set the log level
+		log_level = vim.log.levels.WARN,
 		filetype = {
 			lua = {
 				require("plugin.format.filetype_conf.lua").stylua,
@@ -38,9 +38,9 @@ local config = function()
 			go = {
 				require("plugin.format.filetype_conf.go").goimports,
 			},
-            python = {
-                require("plugin.format.filetype_conf.python").black
-            }
+			python = {
+				require("plugin.format.filetype_conf.python").black,
+			},
 			-- vimwiki = {
 			-- 	require("plugin.format.filetype_conf.markdown").markdownlint,
 			-- },
@@ -52,61 +52,60 @@ augroup FormatAutogroup
   autocmd FileType go autocmd InsertLeave * Format
 augroup END
 ]])
-
 end
 local confi = function()
--- Utilities for creating configurations
-local util = require "formatter.util"
+	-- Utilities for creating configurations
+	local util = require("formatter.util")
 
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup {
-  -- Enable or disable logging
-  logging = true,
-  -- Set the log level
-  log_level = vim.log.levels.WARN,
-  -- All formatter configurations are opt-in
-  filetype = {
-    -- Formatter configurations for filetype "lua" go here
-    -- and will be executed in order
-    lua = {
-      -- "formatter.filetypes.lua" defines default configurations for the
-      -- "lua" filetype
-      require("formatter.filetypes.lua").stylua,
+	-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+	require("formatter").setup({
+		-- Enable or disable logging
+		logging = true,
+		-- Set the log level
+		log_level = vim.log.levels.WARN,
+		-- All formatter configurations are opt-in
+		filetype = {
+			-- Formatter configurations for filetype "lua" go here
+			-- and will be executed in order
+			lua = {
+				-- "formatter.filetypes.lua" defines default configurations for the
+				-- "lua" filetype
+				require("formatter.filetypes.lua").stylua,
 
-      -- You can also define your own configuration
-      function()
-        -- Supports conditional formatting
-        if util.get_current_buffer_file_name() == "special.lua" then
-          return nil
-        end
+				-- You can also define your own configuration
+				function()
+					-- Supports conditional formatting
+					if util.get_current_buffer_file_name() == "special.lua" then
+						return nil
+					end
 
-        -- Full specification of configurations is down below and in Vim help
-        -- files
-        return {
-          exe = "stylua",
-          args = {
-            "--search-parent-directories",
-            "--stdin-filepath",
-            util.escape_path(util.get_current_buffer_file_path()),
-            "--",
-            "-",
-          },
-          stdin = true,
-        }
-      end
-    },
+					-- Full specification of configurations is down below and in Vim help
+					-- files
+					return {
+						exe = "stylua",
+						args = {
+							"--search-parent-directories",
+							"--stdin-filepath",
+							util.escape_path(util.get_current_buffer_file_path()),
+							"--",
+							"-",
+						},
+						stdin = true,
+					}
+				end,
+			},
 
-    -- Use the special "*" filetype for defining formatter configurations on
-    -- any filetype
-    ["*"] = {
-      -- "formatter.filetypes.any" defines default configurations for any
-      -- filetype
-      require("formatter.filetypes.any").remove_trailing_whitespace,
-      -- Remove trailing whitespace without 'sed'
-      -- require("formatter.filetypes.any").substitute_trailing_whitespace,
-    }
-  }
-}
+			-- Use the special "*" filetype for defining formatter configurations on
+			-- any filetype
+			["*"] = {
+				-- "formatter.filetypes.any" defines default configurations for any
+				-- filetype
+				require("formatter.filetypes.any").remove_trailing_whitespace,
+				-- Remove trailing whitespace without 'sed'
+				-- require("formatter.filetypes.any").substitute_trailing_whitespace,
+			},
+		},
+	})
 end
 M.lazy = {
 	"mhartington/formatter.nvim",
