658e8f6bcd38fb0718bab6180fd8558a50468726
illumiko <eyanat.rayannjr@gmail.com>
2026-05-27 22:08:24 +0600

    added buffer=true for keymaps

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -2,6 +2,6 @@
 
 vim.g.mapleader = " "
 local opts = function(x)
-	return { noremap = true, silent = true, desc = x }
+	return { noremap = true, silent = true, desc = x, buffer = true }
 end
 vim.keymap.set("n", "<leader>P", ":!go run %<cr>", opts("run current go file"))
